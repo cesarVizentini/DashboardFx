9b5146638f983947fbc60ee0bdc581bed21bb59e
Cesar Augusto dos Santos Vizentini <69355659+cesarVizentini@users.noreply.github.com>
2021-11-20 16:41:30 -0300

    Update ConexaoBanco.java

--- src/main/java/com/gn/dao/ConexaoBanco.java
+++ src/main/java/com/gn/dao/ConexaoBanco.java
@@ -15,7 +15,7 @@ public class ConexaoBanco {
 
         // --> Configurando usuario e senha para acesso ao banco de dados
         configuracao.setProperty("hibernate.connection.username", "root");
-        configuracao.setProperty("hibernate.connection.password", "qwe123QWE!@#");
+        configuracao.setProperty("hibernate.connection.password", "");
 
         // --> Indicando o mapeamento das classes
         configuracao.addPackage("com.gn.model").addAnnotatedClass(Servico.class);
